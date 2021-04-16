b9a60b0b663f419af92049ca720e66f5753dd107
china-bin <958615915@qq.com>
2021-04-16 10:53:05 +0800

    更新vuepress文档

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vdesjs/demo/',
+    // assetsPublicPath: '/vdesjs/demo/',
 
     /**
      * Source Maps
